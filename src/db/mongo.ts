eb68f6ada2a1d58a8b5bf88087fc6d9c5e741fc1
Garen Yondem <garen@yondem.com>
2019-12-12 13:30:26 +0300

    Connect to mongo

---
+++ src/db/mongo.ts
@@ -0,0 +1,26 @@
+import mongoose, { ConnectionOptions } from 'mongoose';
+import config from '../config';
+import { MONGOOSE_EVENT, NODE_ENV } from '../utils/Enums';
+
+const options: ConnectionOptions = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+};
+
+if (config.node.env == NODE_ENV.DEVELOPMENT) {
+    mongoose.set('debug', true);
+}
+
+mongoose.connection
+    .on(MONGOOSE_EVENT.CONNECTED, () => {
+        console.info('Mongo connection successful');
+    })
+    .on(MONGOOSE_EVENT.ERROR, (err) => {
+        console.error(`Mongo connection error ${err}`);
+    });
+
+const init = async () => {
+    await mongoose.connect(config.mongo.uri, options);
+};
+
+export default init;
---
+++ src/utils/Enums.ts
@@ -0,0 +1,9 @@
+export enum NODE_ENV {
+    DEVELOPMENT = 'development',
+    PRODUCTION = 'production',
+}
+
+export enum MONGOOSE_EVENT {
+    CONNECTED = 'connected',
+    ERROR = 'error',
+}
